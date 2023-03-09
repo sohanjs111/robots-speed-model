bc3b7df6ad0c9e29d59988110ef0a4fc2b1d7889
IROBOT <sohan.saldanha@student.fhws.de>
2023-03-09 17:49:19 +0100

    Updated the python code

--- predictSD.py
+++ predictSD.py
@@ -11,6 +11,9 @@ bot_poly_y2_model = joblib.load("./Models/bot_poly_y2.sav")
 evo_sr_y1_model = joblib.load("./Models/evo_sr_y1.sav")
 evo_sr_y2_model = joblib.load("./Models/evo_sr_y2.sav")
 evo_sr_y3_model = joblib.load("./Models/evo_sr_y3.sav")
+evo_poly_y1_model = joblib.load("./Models/evo_poly_y1.sav")
+evo_poly_y2_model = joblib.load("./Models/evo_poly_y2.sav")
+evo_poly_y3_model = joblib.load("./Models/evo_poly_y3.sav")
 
 # fucntion for predicting SD for LR
 def stand_d_sr(bot_or_evo, drive_on, speed):
@@ -44,39 +47,75 @@ def stand_d_poly(bot_or_evo, drive_on, speed):
       return -1
   if (bot_or_evo == 2):
     if (drive_on == 1):
-      return evo_sr_y1_model.predict(np.array([[speed]]))
+      poly_obj= PolynomialFeatures(degree=2)
+      return evo_poly_y1_model.predict(poly_obj.fit_transform(np.array([[speed]])))
     elif (drive_on == 2):
-      return evo_sr_y2_model.predict(np.array([[speed]]))
+      poly_obj= PolynomialFeatures(degree=2)
+      return evo_poly_y2_model.predict(poly_obj.fit_transform(np.array([[speed]])))
     elif (drive_on == 3):
-      return evo_sr_y3_model.predict(np.array([[speed]]))
+      poly_obj= PolynomialFeatures(degree=2)
+      return evo_poly_y3_model.predict(poly_obj.fit_transform(np.array([[speed]])))
     else:
       return -1
 
 # Which robot?
-print("Estimate the standard deviation for \n 1. TurtleBot \n 2. Evocortex")
-bot_or_evo = input()
-bot_or_evo = int(bot_or_evo)
+def which_robot():
+  print("Estimate the standard deviation for \n 1. TurtleBot \n 2. Evocortex")
+  robot_no = input()
+  robot_no = int(robot_no)
+  if robot_no == 1 or robot_no == 2:
+    return robot_no
+  else: 
+    print("Wrong choice! We only have model for Turtlebot and Evocortex \n")
+    return -1 
+
 
 # driving surface 
-if bot_or_evo == 1: 
-  print("What is the robot driving on? \n 1. Green Carpet \n 2. Floor")
-  drive_on = input()
-  drive_on = int(drive_on)
-elif bot_or_evo == 2:
-  print("What is the robot driving on? \n 1. Green Carpet \n 2. Floor \n 3. Foam")
-  drive_on = input()
-  drive_on = int(drive_on)
-else: 
-  print("Wrong choice! We only have model for Turtlebot and Evocortex")
+def surface_on(bot_or_evo):
+  if bot_or_evo == 1: 
+    print("What is the robot driving on? \n 1. Green Carpet \n 2. Floor")
+    drive_on = input()
+    drive_on = int(drive_on)
+    if drive_on == 1 or drive_on == 2:
+      return drive_on
+    else: 
+      print("Wrong choice! We only have Green carpet and floor SD for Turtlebot \n")
+      return -1 
+  elif bot_or_evo == 2:
+    print("What is the robot driving on? \n 1. Green Carpet \n 2. Floor \n 3. Foam")
+    drive_on = input()
+    drive_on = int(drive_on)
+    if drive_on == 1 or drive_on == 2 or drive_on == 3:
+      return drive_on
+    else: 
+      print("Wrong choice! We only have Green carpet, floor and foam SD for Evocortex \n")
+      return -1 
 
 # Speed of the robot
-print("What is the speed of the Robot?")
-speed = input()
-speed = float(speed)
+def robot_speed():
+  print("What is the speed of the Robot?")
+  speed = input()
+  speed = float(speed)
+  return speed
 
-res = stand_d_poly(bot_or_evo, drive_on, speed)
-#res = stand_d_sr(bot_or_evo, drive_on, speed)
-if res == -1:
-  print("Can not predict for this combination")
-else:
-  print("The predicted standard devitation is", res[0])
+if __name__ == "__main__":
+
+  robot_no = -1
+  while(robot_no == -1):
+    robot_no = which_robot()
+
+  surface_no = -1
+  while(surface_no == -1):
+    surface_no = surface_on(robot_no)
+
+  speed = robot_speed()
+
+
+  res_poly = stand_d_poly(robot_no, surface_no, speed)
+  res_lr = stand_d_sr(robot_no, surface_no, speed)
+  if res_poly == -1 or res_lr == -1:
+    print("Can not predict for this combination")
+  else:
+    print("Using Polynomial Regression the predicted standard devitation is", res_poly[0])
+    print("\n")
+    print("Using Linear Regression the predicted standard devitation is", res_lr[0])
