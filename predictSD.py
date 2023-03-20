baa1a42955221783c2fafba095d7296a99ce481d
Sohan Saldanha <sohanjs111@gmail.com>
2023-03-20 21:22:00 +0100

    Multiple linear regression model added

--- predictSD.py
+++ predictSD.py
@@ -20,6 +20,7 @@ evo_sr_y3_model = joblib.load("./Models/evo_sr_y3.sav")
 evo_poly_y1_model = joblib.load("./Models/evo_poly_y1.sav")
 evo_poly_y2_model = joblib.load("./Models/evo_poly_y2.sav")
 evo_poly_y3_model = joblib.load("./Models/evo_poly_y3.sav")
+evo_ml_model = joblib.load("./Models/evo_ml.sav")
 
 # fucntion for predicting SD for LR
 def stand_d_sr(bot_or_evo, drive_on, speed):
@@ -64,6 +65,10 @@ def stand_d_poly(bot_or_evo, drive_on, speed):
     else:
       return -1
 
+def stand_d_ml(drive_on, speed):
+  y_pred = evo_ml_model.predict([[drive_on,speed]])
+  return y_pred
+
 # Which robot?
 def which_robot():
   print("Estimate the standard deviation for \n 1. TurtleBot \n 2. Evocortex")
@@ -75,6 +80,16 @@ def which_robot():
     print("Wrong choice! We only have model for Turtlebot and Evocortex \n")
     return -1 
 
+# Which algo? 
+def which_algo(): 
+  print("Which model would you like to use \n 1. Multiple linear Reg. \n 2. Simple or Poly linear Reg.")
+  model_no = input()
+  model_no = int(model_no)
+  if model_no == 1 or model_no == 2:
+    return robot_no
+  else: 
+    print("Wrong choice! We only have Multiple linear and Simple or Polynomial Regression model for Evocortex \n")
+    return -1 
 
 # driving surface 
 def surface_on(bot_or_evo):
@@ -104,23 +119,45 @@ def robot_speed():
   speed = float(speed)
   return speed
 
+# Friction coefficient of the surface
+def surface_fc():
+  print("What is the friction coefficient of the surface?")
+  fc = input()
+  fc = float(fc)
+  return fc
+
 if __name__ == "__main__":
   # robot
   robot_no = -1
   while(robot_no == -1):
     robot_no = which_robot()
-  # Surface
-  surface_no = -1
-  while(surface_no == -1):
-    surface_no = surface_on(robot_no)
-  # Speed
-  speed = robot_speed()
-  # model
-  res_poly = stand_d_poly(robot_no, surface_no, speed)
-  res_lr = stand_d_sr(robot_no, surface_no, speed)
-  if res_poly == -1 or res_lr == -1:
-    print("Can not predict for this combination")
+  # algo
+  if robot_no == 2:
+    reg_model = -1
+    while(reg_model == -1):
+      reg_model = which_algo()
+  # For Multiple linear Regression 
+  if reg_model == 2: 
+    speed = robot_speed()
+    friction_coeff = surface_fc()
+    # model for ml 
+    ml_res = stand_d_ml(friction_coeff, speed)
+    print("Using Multiple linear Regression the predicted standard devitation is", ml_res[0])
   else:
-    print("Using Polynomial Regression the predicted standard devitation is", res_poly[0])
-    print("\n")
-    print("Using Linear Regression the predicted standard devitation is", res_lr[0])
+  # Surface
+    surface_no = -1
+    while(surface_no == -1):
+      surface_no = surface_on(robot_no)
+    # Speed
+    speed = robot_speed()
+    # model
+    res_poly = stand_d_poly(robot_no, surface_no, speed)
+    res_lr = stand_d_sr(robot_no, surface_no, speed)
+    if res_poly == -1 or res_lr == -1:
+      print("Can not predict for this combination")
+    else:
+      print("Using Polynomial Regression the predicted standard devitation is", res_poly[0])
+      print("\n")
+      print("Using Linear Regression the predicted standard devitation is", res_lr[0])
+    
+  
